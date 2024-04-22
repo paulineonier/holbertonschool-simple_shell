bdf18ca983e4328a3c227fadb4efdea361f565fc
paulineonier <pauline.onier@outlook.fr>
2024-04-22 13:45:22 +0000

    new

--- path.c
+++ path.c
@@ -1,51 +1,52 @@
 #include "shell.h"
-#include <sys/stat.h>
-#include <string.h>
-#include <stdio.h>
-
 /**
- * get_path - handle the path
- * @line: a string of command
- * Return: the handle the path
+ * _path - Locates the full path of an executable command.
+ *
+ * @command: The command to find the full path for.
+ * Return: A dynamically allocated string containing
+ * the full path of the command.
+ * Returns NULL if the command is not found
+ * or in case of memory allocation failure.
  */
-
-char *path(char *line)
+char *_path(char *command)
 {
-	char *path;
-	int length_line;
-	char *token, *tmp, *file;
-	struct stat st;
+	char *env, *path, *cmd;
+	struct stat buf;
+	int i;
 
-	if (line == NULL)
-		exit(EXIT_FAILURE);
-	length_line = strlen(line);
-	if (strstr(line, "/") == NULL)
+	for (i = 0; command[i]; i++)
 	{
-		path = getenv("PATH");
-		tmp = malloc(strlen(path) + 1);
-		if (tmp == NULL)
-			perror("malloc"), exit(EXIT_FAILURE);
-		strcpy(tmp, path);
-		token = strtok(tmp, ":");
-		while (token)
+		if (command[i] == '/')
 		{
-			file = malloc(length_line + strlen(token) + 2);
-			if (file == NULL)
-			{
-				free(tmp), free(file);
-				perror("malloc"), exit(EXIT_FAILURE);
-			}
-			sprintf(file, "%s/%s", token, line);
-			if (stat(file, &st) == 0)
-			{
-				free(tmp);
-				return (file);
-			}
-			free(file);
-			token = strtok(NULL, ":");
+			if (stat(command, &buf) == 0)
+				return (my_strdup(command));
+			return (NULL);
 		}
-		free(tmp);
-		return (NULL);
 	}
-	return (line);
+	env = get_env("PATH");
+	if (!env)
+		return (NULL);
+	path = strtok(env, ":");
+	while (path)
+	{
+		cmd = malloc(strlen(path) + strlen(command) + 2);
+		if (cmd != NULL)
+		{
+			snprintf(cmd, strlen(path) + strlen(command) + 2, "%s/%s", path, command);
+			if (stat(cmd, &buf) == 0)
+			{
+				free(env);
+				return (cmd);
+			}
+			free(cmd);
+			path = strtok(NULL, ":");
+		}
+		else
+		{
+			free(env);
+			return (NULL);
+		}
+	}
+	free(env);
+	return (NULL);
 }
--- shell.h
+++ shell.h
@@ -1,23 +1,24 @@
 #ifndef SHELL_H
 #define SHELL_H
-
-#include <stddef.h>
-
-void _prompt(void);
-size_t read_input(char **line, size_t *len);
-void remove_newline(char *str);
-char *path(char *line);
-char **get_argument(char *line);
-char *_env(const char *name);
-void execute_command(char **array_command, int nbr_command);
-void free_args(char **array_command);
-void _execute(char *command);
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <sys/stat.h>
 
+extern char **environ;
+char *read_line(void);
+int is_built_in(char **command);
+void handle_built_in(char **command, int *status);
+void hsh_exit(char **cmd, int *status);
+char *_path(char *command);
+char *get_env(char *var);
+char *my_strdup(const char *s);
+char **_str(char *line);
+void free_array(char **command);
+int exec(char **arr, char **av, int idx);
+int main(__attribute__((unused)) int ac, char **av);
+void hsh_env(char **cmd, int *status);
 #endif
